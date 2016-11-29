9fa0f7e5f8f71e716ec91a9b418545048070693e
emilycroft <emilyhcroft@gmail.com>
2016-11-29 16:36:35 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,29 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  result = Hash.new
+
+  data.each do |attribute, hashes|
+    hashes.each do |key, array|
+      array.each do |element|
+        result[element] = {color: [], gender: [], lives: []}
+      end
+    end
+  end
+
+  data.each do |attribute, hashes|
+    hashes.each do |key, array|
+      array.each do |element|
+        if attribute == :color
+          result[element][:color] << key.to_s
+        elsif attribute == :gender
+          result[element][:gender] << key.to_s
+        elsif attribute == :lives
+          result[element][:lives] << key.to_s
+        end
+      end
+    end
+  end
+  puts result
+  result
 end
