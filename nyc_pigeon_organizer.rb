99387128ee4f5dfecac6d3ac7b1a641c2823f492
emilycroft <emilyhcroft@gmail.com>
2016-12-03 15:11:04 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,29 +1,48 @@
-require 'pry'
-
 def nyc_pigeon_organizer(data)
-  result = Hash.new
+  new_hash = {}
 
-  data.each do |attribute, hashes|
-    hashes.each do |key, array|
-      array.each do |element|
-        result[element] = {color: [], gender: [], lives: []}
+  data.each do |attributes, data_hash|
+    data_hash.each do |data_name, names_array|
+      names_array.each do |name|
+        new_hash[name] ||= {}
+        new_hash[name][attributes] ||= []
+        new_hash[name][attributes] << data_name.to_s
       end
     end
   end
 
-  data.each do |attribute, hashes|
-    hashes.each do |key, array|
-      array.each do |element|
-        if attribute == :color
-          result[element][:color] << key.to_s
-        elsif attribute == :gender
-          result[element][:gender] << key.to_s
-        elsif attribute == :lives
-          result[element][:lives] << key.to_s
-        end
-      end
-    end
-  end
-  puts result
-  result
+  new_hash
 end
+
+
+
+
+# require 'pry'
+#
+# def nyc_pigeon_organizer(data)
+#   result = Hash.new
+#
+#   data.each do |attribute, hashes|
+#     hashes.each do |key, array|
+#       array.each do |element|
+#         result[element] = {color: [], gender: [], lives: []}
+#       end
+#     end
+#   end
+#
+#   data.each do |attribute, hashes|
+#     hashes.each do |key, array|
+#       array.each do |element|
+#         if attribute == :color
+#           result[element][:color] << key.to_s
+#         elsif attribute == :gender
+#           result[element][:gender] << key.to_s
+#         elsif attribute == :lives
+#           result[element][:lives] << key.to_s
+#         end
+#       end
+#     end
+#   end
+#   puts result
+#   result
+# end
